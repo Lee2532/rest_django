46dd4011edb69975d89a84b54107c94ebd610131
mojoblue7 <lsh18542@gmail.com>
2020-08-29 04:48:44 +0900

    feat: travel/page/ URL 렌더링 구현. page 퍼블리싱

--- travel/urls.py
+++ travel/urls.py
@@ -8,4 +8,5 @@ urlpatterns = format_suffix_patterns([
     path('<int:pk>/', views.TravelDetail.as_view(), name='Travel-detail'),
     path('test/', views.TravelTestView.as_view(), name='Travel-detail'),
     path('test/<int:pk>/', views.TravelTestDetailVeiw.as_view(), name='Travel-detail'),
+    path('page/', views.TravelTemplateView.as_view(), name="Travel-template")
 ])
