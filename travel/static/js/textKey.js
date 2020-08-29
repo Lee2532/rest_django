13ba793a765cf5518c1e7a539c30de0c73f028e5
mojoblue7 <lsh18542@gmail.com>
2020-08-30 02:47:15 +0900

    feat: GET, POST call 페이지 구현

---
+++ travel/static/js/textKey.js
@@ -0,0 +1,3 @@
+class TextKey {};
+
+TextKey.wrongRequest = "삐빅?! 잘못된 인수나, URL을 입력하셨습니다.";
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'jobsearch.apps.JobsearchConfig',
     'travel.apps.TravelConfig',
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'tutorial.urls'
@@ -93,6 +95,13 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+# CORS Settings
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
